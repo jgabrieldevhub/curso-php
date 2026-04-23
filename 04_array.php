a47c3655709eda2de06d28badb7110a29a4336fe
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-04-22 21:54:46 -0300

    php array

---
+++ 04_array.php
@@ -0,0 +1,12 @@
+<?php
+    $carros = ['Uno', 'Gol', 'Fusion', 'Civic'];
+
+    print "$carros[1]"."<br>"."$carros[2]";
+
+    echo "<br>";
+
+    $carroUm = ['Uno', 'Fiat', 'Branco'];
+
+    print "$carroUm[0]"."<br>"."$carroUm[1]"."<br>"."$carroUm[2]";
+
+?>
