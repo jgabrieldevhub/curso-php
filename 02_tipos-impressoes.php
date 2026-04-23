775cb116c92c8c3e819c7458280d99ceb0dc504d
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-04-22 21:53:44 -0300

    php var

---
+++ 02_tipos-impressoes.php
@@ -0,0 +1,24 @@
+<?php
+
+    $music = 'a miserable life...';
+
+    print "I like to listen $music";
+
+    echo '<br>';
+    echo '<br>';
+    
+    $singer = 'Sombr';
+
+    print "I like to listen Back to Friends of $singer";
+
+    echo '<br>';
+    echo '<br>';
+
+    $band = 'Skillet';
+
+    print "i like to listen the $band songs";
+
+    echo '<br>';
+    echo '<br>';
+    
+?>
