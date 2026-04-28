5f9b034eb06ef67876aa2cc6a86590dcb62468dc
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-04-27 22:11:04 -0300

    manipulations with arrays

---
+++ 06_manipulacoes_arrays.php
@@ -0,0 +1,39 @@
+<?php
+    $carrinho = [
+        'Arroz',
+        'Sabão',
+        'feijão',
+        'Balinhas',
+        'Balinhas',
+    ];
+
+    echo "<pre>";
+    echo "<hr>";
+    echo "<br>";
+
+    /*
+        array_pop = "Apaga o ultimo valor do array";
+        array_shift = "Apaga o primeiro valor do array";
+        array_push = "Adiciona elementos no array";
+        array_unshift = "Adiciona um elemento como prinmeiro valor da array";
+        array_unique = "Remove valores duplicados e retorna Único";
+        unset = "Remove um valor do array";
+    */
+
+    /* 
+        array_pop($carrinho);
+        array_shift($carrinho);
+        unset($carrinho[0]);
+        array_push($carrinho, 'Tapete');
+        array_push($carrinho, 'Rodo');
+        array_unshift($carrinho, 'Microondas');
+        array_unshift($carrinho, 'Tapete');
+        $carrinho = array_unique($carrinho);
+    */
+    
+    
+    var_dump($carrinho);
+
+    echo "<br>";
+    echo "<hr>";
+?>
