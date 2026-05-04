8ef3587d4f2ef321a479baa197575111e514dcb9
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-04 19:48:47 -0300

    concatenacao com php

---
+++ 10_concatenacao.php
@@ -0,0 +1,13 @@
+<?php 
+    $var1 = 'João';
+    $var2 = ' da Silva';
+
+    echo $var1 .''. $var2 .''. 12;
+
+    echo '<br>';
+
+    $nome = 'Rubens Pereira';
+    $empresa = 'Senac Americana';
+
+    echo "O {$nome} é fundador da Empresa: {$empresa}";
+?>
