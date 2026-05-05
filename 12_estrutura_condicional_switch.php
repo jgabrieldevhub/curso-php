3762b159537c9dcee0805e7c0d962ced4a248c14
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-04 21:21:27 -0300

    switch php

---
+++ 12_estrutura_condicional_switch.php
@@ -0,0 +1,21 @@
+<?php
+    $numero = 12;
+
+    switch ($numero) {
+        case $numero == 12;
+            echo 'Doze';
+        break;
+
+        case $numero == 13;
+            echo 'Treze';
+        break;
+
+        case $numero == 14;
+            echo 'Quartoze';
+        break;
+
+        default;
+            echo 'Valor não encontrado';
+        break;
+    }
+?>
