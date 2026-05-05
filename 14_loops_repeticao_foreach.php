47f5b3f5709b957c0411661af527d991a62cfcf6
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-04 22:11:19 -0300

    foreach php

---
+++ 14_loops_repeticao_foreach.php
@@ -0,0 +1,16 @@
+<?php
+    $nomes = [
+        'aluno1' => 'Erica',
+        'aluno2' => 'Ana',
+        'aluno3' => 'Igor',
+        'aluno4' => 'Leandro',
+    ];
+
+    foreach ($nomes as $k => $nomes) {
+        echo "{$nomes} <br>";
+    }
+
+    /*
+        k = 'chave';
+    */
+?>
