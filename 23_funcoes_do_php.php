59e0e39a190f9a60af22ae058c474fd240f80fde
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-05 21:58:27 -0300

    function isset php

---
+++ 23_funcoes_do_php.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = 'Carlos Ferreira';
+
+    if(isset($nome)){
+        echo $nome;
+    }else{
+        echo 'Não Existe';
+    }
+
+    /*
+        isset = 'Valida a existencia de uma variavel';
+    */
+?>
