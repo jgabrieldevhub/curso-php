cb95645f9c21103d8fc78463c10970a198d990d8
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-05 22:17:18 -0300

    function date_defaut_timezone_set php

---
+++ 24_funcoes_do_php.php
@@ -0,0 +1,10 @@
+<?php
+    date_default_timezone_set('America/Sao_Paulo');
+
+    echo 'O ano Atual: ' . date('Y') .'<br>';
+    echo 'O mês Atual: ' . date('m') .'<br>';
+    echo 'O dia Atual: ' . date('d') .'<br>';
+
+    echo 'A data Atual: ' . date('d/m/Y') .'<br>';
+    echo 'Hora Atual: ' . date('H:i:s') .'<br>';
+?>
