62ad05eaad9de83ed13813dd565116c862932ab9
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-05 21:55:35 -0300

    function ltrim, rtrim and trim php

---
+++ 21_funcoes_do_php.php
@@ -0,0 +1,20 @@
+<?php
+    $nome = ' Senac Americana ';
+    var_dump($nome);
+    echo '<hr>';
+
+    var_dump(ltrim($nome));
+    echo '<hr>';
+
+    /* ltrim = 'remove os espaços do lado esquerdo'; */
+
+    var_dump(rtrim($nome));
+    echo '<hr>';
+
+    /* rtrim = 'remove os espaços do lado direito'; */
+
+    var_dump(trim($nome));
+    echo '<hr>';
+
+    /* trim = 'remove os espaços de ambos os lados'; */
+?>
