cfc228d46d03d21b46b121f8324cce9df0cd94f8
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-05-05 21:51:38 -0300

    function explode php

---
+++ 19_funcoes_do_php.php
@@ -0,0 +1,10 @@
+<?php
+    $estado = 'São Paulo/SP';
+    $arrayEstado = explode('/', $estado);
+    echo "<pre>";
+    print_r($arrayEstado);
+
+    /*
+        explode = 'recorta e retorna em vaores de array';
+    */
+?>
